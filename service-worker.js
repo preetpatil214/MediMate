3e7dc487176194510e3d31f7e5f46ac0c050eb7a
Preet <preetpatil214@gmail.com>
2026-08-30 00:30:06 +0530

    test36

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "medimate-v1";
+const CACHE_NAME = "medimate-v3";
 
 const APP_FILES = [
     "./",
@@ -9,53 +9,76 @@ const APP_FILES = [
     "./med1.png",
     "./med2.png",
     "./med3.png",
-    "./med4.png"
+    "./med4.png",
+    "./pillicon.png",
+    "./icon.png",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./alarm.mp3"
 ];
 
-/* Install service worker */
 self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(APP_FILES);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+            .then(() => self.skipWaiting())
     );
-
-    self.skipWaiting();
 });
 
-/* Activate service worker */
 self.addEventListener("activate", event => {
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames
-                    .filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-            );
-        })
+        caches.keys()
+            .then(cacheNames =>
+                Promise.all(
+                    cacheNames
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
+                )
+            )
+            .then(() => self.clients.claim())
     );
-
-    self.clients.claim();
 });
 
-/* Fetch cached app files */
 self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
-        caches.match(event.request).then(cachedResponse => {
-            return cachedResponse || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(cachedResponse => {
+                if (cachedResponse) return cachedResponse;
+
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (
+                            response &&
+                            response.status === 200 &&
+                            response.type === "basic"
+                        ) {
+                            const copy = response.clone();
+
+                            caches.open(CACHE_NAME)
+                                .then(cache => cache.put(event.request, copy))
+                                .catch(() => {});
+                        }
+
+                        return response;
+                    })
+                    .catch(() => caches.match("./index.html"));
+            })
     );
 });
 
-/* Handle notification clicks */
 self.addEventListener("notificationclick", event => {
+
     event.notification.close();
 
     event.waitUntil(
         clients.matchAll({
             type: "window",
             includeUncontrolled: true
-        }).then(clientList => {
+        })
+        .then(clientList => {
 
             for (const client of clientList) {
                 if ("focus" in client) {
@@ -66,6 +89,8 @@ self.addEventListener("notificationclick", event => {
             if (clients.openWindow) {
                 return clients.openWindow("./index.html");
             }
+
+            return undefined;
         })
     );
 });
