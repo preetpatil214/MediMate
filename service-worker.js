d438d58e7d0bdfcdbe3f28661369782e72d33373
Preet <preetpatil214@gmail.com>
2026-08-29 20:09:24 +0530

    notification and alarm test

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+const CACHE_NAME = "medimate-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./addmed.html",
+    "./manifest.json",
+    "./bgmain.jpg",
+    "./med1.png",
+    "./med2.png",
+    "./med3.png",
+    "./med4.png"
+];
+
+/* Install service worker */
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(APP_FILES);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+/* Activate service worker */
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+/* Fetch cached app files */
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+/* Handle notification clicks */
+self.addEventListener("notificationclick", event => {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({
+            type: "window",
+            includeUncontrolled: true
+        }).then(clientList => {
+
+            for (const client of clientList) {
+                if ("focus" in client) {
+                    return client.focus();
+                }
+            }
+
+            if (clients.openWindow) {
+                return clients.openWindow("./index.html");
+            }
+        })
+    );
+});
